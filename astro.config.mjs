1370c7912033967e859578fbe1b2a5e9f041b660
Luan Coelho <luancoelho.dev@gmail.com>
2026-01-21 15:22:02 -0300

    Speed Insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,19 @@
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
-
+import vercel from '@astrojs/vercel/serverless'
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.padresparapalmas.com.br',
-  integrations: [sitemap(), react()],
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: 'https://www.padresparapalmas.com.br',
+    integrations: [sitemap(), react()],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true // set to false when using @vercel/analytics@1.4.0
+        }
+    })
 })
