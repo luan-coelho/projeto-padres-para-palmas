8a33b8b1e7711fd2377e9349f6d18e0e941a3cd8
Luan Coêlho <luancoelho.dev@gmail.com>
2026-02-01 12:03:52 -0300

    feat: Configure Astro for Vercel deployment by enabling the adapter and setting trailing slash behavior.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
-// import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel'
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.padresparapalmas.com.br',
+  // site: 'https://www.padresparapalmas.com.br',
   output: 'static',
+  trailingSlash: 'never',
   integrations: [sitemap(), react()],
   vite: {
     plugins: [tailwindcss()]
-  }
-  // adapter: vercel()
+  },
+  adapter: vercel()
 })
